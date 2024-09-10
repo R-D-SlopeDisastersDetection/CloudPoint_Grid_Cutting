d5262f2b01ed9fbf024de80548089bdc80381ec0
jiduowang <1963760354@qq.com>
2024-09-11 00:01:23 +0800

    修复一些已知的错误

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     point_cloud = o3d.io.read_point_cloud("../dataset_reg/simtest/5331ori.ply")
     x_block = 5
     y_block = 5
-    cloud_point_grid_cutting = CloudPointGridCutting(x_block, y_block, point_cloud, 'output')
+    cloud_point_grid_cutting = CloudPointGridCutting(x_block, y_block, point_cloud, 'output', cut_type='block_size')
     blocks = cloud_point_grid_cutting.grid_cutting()
     cloud_point_grid_cutting.output_files()
